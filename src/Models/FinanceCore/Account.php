cce70358f632427980dde9353a6b33445d0fab19
Farabi Djauhari <farabi.djauhari@brighte.com.au>
2022-12-12 18:26:10 +1100

    fix(SHLD-826): add finance account query

---
+++ src/Models/FinanceCore/Account.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrighteCapital\Api\Models\FinanceCore;
+
+class Account
+{
+
+    /** @var string Account ID */
+    public $id;
+
+    /** @var int Loan Type Id */
+    public $loanTypeId;
+
+    /** @var int Vendor Id */
+    public $vendorId;
+
+    /** @var string Status */
+    public $status;
+
+    /** @var VendorRebate[]  Rebates */
+    public $rebates;
+
+}
