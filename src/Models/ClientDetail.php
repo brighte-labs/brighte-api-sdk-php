afe6577a1d2705611c7f183b481680d64b5628db
Selmer del Rosario <selmer.anthony@brighte.com.au>
2024-06-13 07:43:04 +0800

    feat(CT-434): get client details

---
+++ src/Models/ClientDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrighteCapital\Api\Models;
+
+class ClientDetail
+{
+
+    /** @var string */
+    public $dateOfBirth;
+
+    /** @var string */
+    public $firstName;
+
+    /** @var string */
+    public $middleName;
+    
+    /** @var string */
+    public $lastName;
+
+}
